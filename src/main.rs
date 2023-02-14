311fcfd4a4bc5c61680f25d6aff47fcf192ac571
Rahul <basicfunc@gmail.com>
2023-02-14 12:58:20 +0530

    Wrapped up in a struct and methods.

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,86 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
-struct Section {
-    name: String,
-    values: Vec<HashMap<String, String>>,
+#[derive(Debug, Clone)]
+struct Section(HashMap<String, Option<Vec<HashMap<String, String>>>>);
+
+impl Section {
+    fn new() -> Self {
+        Self(HashMap::new())
+    }
 }
 
+#[derive(Debug, Clone)]
 struct Ini {
-    bytes: String,
-    sections: Vec<Section>,
+    stream: String,
+    sections: Section,
 }
 
 impl Ini {
     fn new(bytes: String) -> Self {
         Self {
-            bytes,
-            sections: vec![],
+            stream: bytes,
+            sections: Section::new(),
         }
     }
 
-    fn remove_comments(self) -> String {
-        let mut result = String::new();
+    fn remove_comments(&mut self) {
+        let mut uncommented_src = String::new();
 
-        for line in self.bytes.lines() {
+        for line in self.stream.lines() {
             let line = line.trim();
 
             if let Some(idx) = line.find(';') {
-                result.push_str(&line[0..idx]);
+                uncommented_src.push_str(&line[0..idx]);
             } else if let Some(idx) = line.find('#') {
-                result.push_str(&line[0..idx]);
+                uncommented_src.push_str(&line[0..idx]);
             } else {
-                result.push_str(line)
+                uncommented_src.push_str(line)
             }
 
-            result.push('\n');
+            uncommented_src.push('\n');
         }
 
-        result
+        self.stream = uncommented_src
+    }
+
+    fn parse(&mut self) -> Result<Section, String> {
+        let lines = self.stream.lines();
+        let mut section = String::new();
+        let mut values: Vec<HashMap<String, String>> = vec![];
+
+        for (idx, line) in lines.into_iter().enumerate() {
+            let line = line.trim();
+
+            if line == "\n" || line.is_empty() {
+                continue;
+            } else if line.starts_with('[') && line.ends_with(']') {
+                if !(section.is_empty()) {
+                    if values.is_empty() {
+                        self.sections.0.insert(section, None);
+                    } else {
+                        self.sections.0.insert(section, Some(values.clone()));
+                    }
+                }
+                section = line[1..line.len() - 1].into();
+                values = vec![];
+            } else {
+                let parts: Vec<&str> = line.split('=').collect();
+
+                if parts.len() != 2 {
+                    return Err(format!(
+                        "Error: Expected \"key = value\" pair at line number {:?}.",
+                        idx + 1
+                    ));
+                } else {
+                    let mut pairs: HashMap<String, String> = HashMap::new();
+                    pairs.insert(parts[0].into(), parts[1].into());
+
+                    values.push(pairs);
+                }
+            }
+        }
+        Ok(self.sections.to_owned())
     }
 }
 
@@ -57,8 +101,11 @@ fn open_ini(filename: &String) -> Result<String, std::io::Error> {
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() == 2 {
-        let res = Ini::new((open_ini(&args[1])).unwrap());
-        println!("{:?}", res.remove_comments());
+        let mut res = Ini::new((open_ini(&args[1])).unwrap());
+        res.remove_comments();
+        let parse = res.parse().unwrap();
+
+        println!("{:?}", parse.0.keys());
     } else {
         eprintln!("\x1B[1m\x1B[31mError: Wrong number of argument passed!\nExpected: {:?} <filename.ini>\x1B[0m",
             args[0]
